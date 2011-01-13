1784ad8a41fd78dd4831accbc7e54de0f5471ade
Jamie van Dyke <jamie@fearoffish.com>
2011-01-13 17:00:11 +0000

    bump version

--- lib/rehabilitate.rb
+++ lib/rehabilitate.rb
@@ -2,7 +2,7 @@ require 'rehabilitate/plugin'
 require 'rehabilitate/plugins/splitter'
 
 class Rehabilitate
-  VERSION = "0.3.12"
+  VERSION = "0.3.13"
   
   def self.backup(options)
     driver = Plugin::create( options.driver )
